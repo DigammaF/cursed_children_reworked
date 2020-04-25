f50ed30ef77b861d0acf950a31b8c5c9060cc36c
DigammaForrun <digamma.for@gmail.com>
2020-04-25 01:33:20 +0100

    Initialisation

---
+++ Assets/scripts/main_menu_play_button.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_menu_play_button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Clicked() {
+
+    	Debug.Log("Go!");
+    	SceneManager.LoadScene("game");
+
+    }
+
+}

b58cef01feb73c075fe88643e325b0288c67f8d8
DigammaForrun <digamma.for@gmail.com>
2020-05-03 13:43:48 +0100

    Ajout de sister_handler

--- Assets/scripts/sister_handler.cs
+++ Assets/scripts/sister_handler.cs
@@ -6,12 +6,20 @@ public class sister_handler : MonoBehaviour
 {
 
 	private bar_handler health_bar_handler;
+	private entity_handler base_entity_handler;
 
     // Start is called before the first frame update
     void Start()
     {
 
     	health_bar_handler = transform.Find("health_bar_canvas").GetComponent<bar_handler>();
+    	base_entity_handler = GetComponent<entity_handler>();
+
+    }
+
+    void UpdateHealthBar() {
+
+    	health_bar_handler.ShadowSet(base_entity_handler.health, base_entity_handler.max_health);
 
     }
 

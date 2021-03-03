4db8144d2d610322b1b86faa4de9f83558f19696
DigammaF <digamma.for@gmail.com>
2021-03-03 15:35:36 +0100

    ajout des points d'interêt

---
+++ Assets/scripts/waypoint_handler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypoint_handler : MonoBehaviour
+{
+
+	private fade_handler fade_hdl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    	fade_hdl = GetComponent<fade_handler>();
+    	Invoke("FadeOut", 1f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FadeOut() {
+
+    	fade_hdl.StartFadeOut();
+
+    }
+
+}
